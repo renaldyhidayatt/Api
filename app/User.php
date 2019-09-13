86d3c8f99b33996883ba6bec91fa6efff713fd16
renaldyhidayatt <renaldyhiayatt@gmail.com>
2019-09-13 23:27:53 +0700

    Passport Package

--- app/User.php
+++ app/User.php
@@ -5,12 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
+    /**HasApiTokensHasApiTokensHasApiTokens
      * The attributes that are mass assignable.
      *
      * @var array
