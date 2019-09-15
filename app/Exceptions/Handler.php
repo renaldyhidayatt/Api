52b3772181aa727d0efc19f6649b70c1a0d621bd
renaldyhidayatt <renaldyhiayatt@gmail.com>
2019-09-15 15:56:53 +0700

    Product Not Belongs to users

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace App\Exceptions;
-
+use App\Exceptions\ExceptionTrait;
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -18,7 +14,6 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
     ];
-
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -28,9 +23,10 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
-
     /**
      * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
      * @param  \Exception  $exception
      * @return void
@@ -39,7 +35,6 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
-
     /**
      * Render an exception into an HTTP response.
      *
@@ -49,19 +44,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($request->expectsJson()){
-            if($exception instanceof ModelNotFoundException){
-                return response()->json([
-                'errors' => 'Product Model Not Found'
-                ],Response::HTTP_NOT_FOUND);
-            }
-            if($exception instanceof NotFoundHttpException){
-                return response()->json([
-                'errors' => 'Incorection Keyword Oke'
-                ],Response::HTTP_NOT_FOUND);
-            }
+        if ($request->expectsJson()) {
+            return $this->apiException($request,$exception);
         }
-        //dd($exceptionn);
         return parent::render($request, $exception);
     }
 }
