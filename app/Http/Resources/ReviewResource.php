ab3382c00de30fc60f5cacb206c3f5356eedf236
renaldyhidayatt <renaldyhiayatt@gmail.com>
2019-09-15 17:43:55 +0700

    Completed Update/Destroy Api

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -1,20 +1,18 @@
 <?php
-
 namespace App\Http\Resources;
-
-use Illuminate\Http\Resources\Json\JsonResource;
-
-class ReviewResource extends JsonResource
+use Illuminate\Http\Resources\Json\Resource;
+class ReviewResource extends Resource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'customer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star
